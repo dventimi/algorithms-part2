fa6caceeca43ace35f33d83d2e08e94919d3b39b
David A. Ventimiglia <dventimi@gmail.com>
2017-11-01 06:04:35 -0700

    Finished SAP, but who knows if it works

--- Week2/src/main/java/SAP.java
+++ Week2/src/main/java/SAP.java
@@ -3,28 +3,57 @@ import java.util.*;
 
 public class SAP {
     Digraph graph = null;
-    // BreadthFirstDirectedPaths paths = null;
+
+    // do unit testing of this class
+    public static void main (String[] args) {
+    }
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP (Digraph G) {
 	if (G==null) throw new IllegalArgumentException();
-	graph = G;
-    }
+	graph = G;}
 
-    // length of shortest ancestral path between v and w; -1 if no such path
-    public int length (int v, int w) {
+    protected int[] research (int v, int w) {
 	BreadthFirstDirectedPaths pv = new BreadthFirstDirectedPaths(graph, v);
 	BreadthFirstDirectedPaths pw = new BreadthFirstDirectedPaths(graph, w);
 	int dw = -1, dv = -1;
+	int aw = -1, av = -1;
 	for (int i : ancestors(graph, v))
-	    if (pw.hasPathTo(i))
+	    if (pw.hasPathTo(i)) {
 		dv = pv.distTo(i) + pw.distTo(i);
+		av = i;}
 	for (int i : ancestors(graph, w))
-	    if (pv.hasPathTo(i))
+	    if (pv.hasPathTo(i)) {
 		dw = pv.distTo(i) + pw.distTo(i);
-	if (dv*dw>0) return -1;
-	return dv < dw ? dv : dw;
-    }
+		aw = i;}
+	if (dv<0 && dw<0) return new int[]{-1, -1};
+	return dv < dw ? new int[]{av, dv} : new int[]{aw, dw};}
+
+    protected int[] research (Iterable<Integer> v, Iterable<Integer> w) {
+	int[] shortest = new int[]{-1, -1};
+	for (int i : v)
+	    for (int j : w) {
+		int[] r = research(i, j);
+		if (r[1]>0 && shortest[1]<0) shortest = r;
+		if (r[1]>0 && shortest[1]>0)
+		    if (r[1]<shortest[1]) shortest = r;}
+	return shortest;}
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length (Iterable<Integer> v, Iterable<Integer> w) {
+	return research(v, w)[1];}
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor (Iterable<Integer> v, Iterable<Integer> w) {
+	return research(v, w)[0];}
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length (int v, int w) {
+	return length(Arrays.asList(v), Arrays.asList(w));}
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor (int v, int w) {
+	return ancestor(Arrays.asList(v), Arrays.asList(w));}
 
     Iterable<Integer> ancestors (final Digraph g, final int s) {
 	return new Iterable<Integer> () {
@@ -57,24 +86,5 @@ public class SAP {
 			    v = q.dequeue();
 			    a = g.adj(v).iterator();
 			    return next();}
-			throw new IllegalStateException();}};}};}
+			throw new IllegalStateException();}};}};}}
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor (int v, int w) {
-	throw new UnsupportedOperationException();
-    }
-
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length (Iterable<Integer> v, Iterable<Integer> w) {
-	throw new UnsupportedOperationException();
-    }
-
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor (Iterable<Integer> v, Iterable<Integer> w) {
-	throw new UnsupportedOperationException();
-    }
-
-    // do unit testing of this class
-    public static void main (String[] args) {
-    }
-}
